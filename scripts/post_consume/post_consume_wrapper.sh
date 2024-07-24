600df7dbd89852d9ba13bd8afccca6f55d653520
Tigeriltis <tigeriltis@users.noreply.github.com>
2024-07-24 09:45:59 +0200

    Rename
    consumption -> consume

--- scripts/post_consume/post_consume_wrapper.sh
+++ scripts/post_consume/post_consume_wrapper.sh
@@ -6,6 +6,6 @@
 SCRIPT_PATH=$(readlink -f "$0")
 SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
 
-echo "This is post consumption wrapper ${SCRIPT_PATH}."
+echo "This is post consume wrapper ${SCRIPT_PATH}."
 
-python3 ${SCRIPT_DIR}/post_consumption_date.py
+python3 ${SCRIPT_DIR}/post_consume_date.py
