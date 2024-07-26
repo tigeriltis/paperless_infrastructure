5671cd4ab178c73bf6d4c2e09de55895d2fcd75e
tigeriltis <tigeriltis@users.noreply.github.com>
2024-07-26 09:57:26 +0200

    added rtf as format I want to consume

--- scripts/consume/paperless_consume_rsync_supported_filetypes.sh
+++ scripts/consume/paperless_consume_rsync_supported_filetypes.sh
@@ -6,4 +6,4 @@ if [ $# -eq 0 ] ; then echo Give at least one file or folder to consume as argum
 ## include *.<extension>  -> supported and wanted filetypes
 ## include */             -> parse all directories
 ## exclude *              -> anything else
-rsync -av --exclude=~* --exclude=.git --include=*/ --include=\*.{pdf,jpeg,jpg,png,tif,gif,txt,odt,ods,odp,doc,docx,ppt,pptx,xls,xlsx,eml} --exclude=\* $@ "${CONSUME_DIR}"
+rsync -av --exclude=~* --exclude=.git --include=*/ --include=\*.{pdf,jpeg,jpg,png,tif,gif,txt,rtf,odt,ods,odp,doc,docx,ppt,pptx,xls,xlsx,eml} --exclude=\* $@ "${CONSUME_DIR}"
